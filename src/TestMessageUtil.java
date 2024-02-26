fde5b3a34cc11ef43e3315017bf81c2795ff3cf8
Yong Tao <117978688+taoyong-ty@users.noreply.github.com>
2024-02-26 11:50:46 -0800

    Update TestMessageUtil.java

--- src/TestMessageUtil.java
+++ src/TestMessageUtil.java
@@ -28,5 +28,58 @@ public class TestMessageUtil {
     messageUtil.setMessage(new String(newMsg));
     assertEquals(newMsg, messageUtil.printMessage());
   }
+
+  @Test
+  public void testPrintMessage1() {	
+    System.out.println("Inside testPrintMessage()");     
+    assertEquals(message,messageUtil.printMessage());
+  }
+
+  @Test
+  public void testSalutationMessage1() {
+    System.out.println("Inside testSalutationMessage()");
+    message = "Hi!" + "Robert";
+    assertEquals(message,messageUtil.salutationMessage());
+  }
+
+
+  @Test
+  public void testSetMessage1() {
+    System.out.println("Inside testSetMessage()");
+    String newMsg = "John";
+    messageUtil.setMessage(new String(newMsg));
+    assertEquals(newMsg, messageUtil.printMessage());
+  }
+
+  @Test
+  public void testPrintMessage2() {	
+    System.out.println("Inside testPrintMessage()");     
+    assertEquals(message,messageUtil.printMessage());
+  }
+
+  @Test
+  public void testSalutationMessage2() {
+    System.out.println("Inside testSalutationMessage()");
+    message = "Hi!" + "Robert";
+    assertEquals(message,messageUtil.salutationMessage());
+  }
+
+
+  @Test
+  public void testSetMessage2() {
+    System.out.println("Inside testSetMessage()");
+    String newMsg = "John";
+    messageUtil.setMessage(new String(newMsg));
+    assertEquals(newMsg, messageUtil.printMessage());
+  }
+
+  @Test
+  public void testSetMessage3() {
+    System.out.println("Inside testSetMessage()");
+    String newMsg = "John";
+    messageUtil.setMessage(new String(newMsg));
+    assertEquals(newMsg, messageUtil.printMessage());
+  }
+  
 }
 
